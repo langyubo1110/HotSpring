85cb2c19a919fc3ee318f5fb15e1696836b9b436
PCX <2314581826@qq.com>
2024-04-25 10:49:26 +0800

    商品库存表实体创建

--- HotSpringProject.Entity/Repo_Goods_Stock.cs
+++ HotSpringProject.Entity/Repo_Goods_Stock.cs
@@ -8,6 +8,13 @@ namespace HotSpringProject.Entity
 {
     public class Repo_Goods_Stock
     {
+        public  int id { get; set; }
+        public string goods_name { get; set; }
+        public int goods_number { get; set; }
+        public string threshold { get; set; }
+        public string factory { get; set; }
+        public string picture { get; set; }
+        public DateTime create_time { get; set; }
 
     }
 }
---
+++ HotSpringProjectRepository/Interface/IRepo_Goods_Stock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotSpringProjectRepository.Interface
+{
+    public interface IRepo_Goods_Stock
+    {
+
+    }
+}
