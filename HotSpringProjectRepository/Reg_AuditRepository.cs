5dee76705453f0e9149dcdc082c97d2e4a61381e
糕手666 <2323886279@qq.com>
2024-04-25 11:58:49 +0800

    接口，注入

---
+++ HotSpringProjectRepository/Reg_AuditRepository.cs
@@ -0,0 +1,13 @@
+﻿using HotSpringProjectRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotSpringProjectRepository
+{
+    internal class Reg_AuditRepository: IReg_AuditRepository
+    {
+    }
+}
