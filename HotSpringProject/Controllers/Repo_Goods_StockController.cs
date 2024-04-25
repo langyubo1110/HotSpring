b966ed825c0f4107a16d0ffd9ae65e69c84804b7
PCX <2314581826@qq.com>
2024-04-25 11:23:44 +0800

    商品库存表控制器创建

---
+++ HotSpringProject/Controllers/Repo_Goods_StockController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotSpringProject.Controllers
+{
+    public class Repo_Goods_StockController : Controller
+    {
+        /* 商品库存表控制器
+         * 裴晨旭
+         * 2024-04-25
+         */
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
