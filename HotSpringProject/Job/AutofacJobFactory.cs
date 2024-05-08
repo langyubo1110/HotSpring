601bd8c33beca1730f15f2e7cf5d10b4ac8eb22a
liuxingyu <2108319749@qq.com>
2024-05-08 14:39:44 +0800

    数据容灾

---
+++ HotSpringProject/Job/AutofacJobFactory.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using Quartz;
+using Quartz.Spi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotSpringProject.Job
+{
+    public class AutofacJobFactory : IJobFactory
+    {
+        private readonly ILifetimeScope _lifetimeScope;
+
+        public AutofacJobFactory(ILifetimeScope lifetimeScope)
+        {
+            _lifetimeScope = lifetimeScope ?? throw new ArgumentNullException(nameof(lifetimeScope));
+        }
+
+        public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
+        {
+            try
+            {
+                var jobDetail = bundle.JobDetail;
+                var jobType = jobDetail.JobType;
+                return (IJob)_lifetimeScope.Resolve(jobType);
+            }
+            catch (Exception ex)
+            {
+                throw new SchedulerException($"Problem while instantiating job '{bundle.JobDetail.Key}'", ex);
+            }
+        }
+
+        public void ReturnJob(IJob job)
+        {
+            // Optionally, you can release resolved job instances back to the container here.
+            (job as IDisposable)?.Dispose();
+        }
+    }
+}
