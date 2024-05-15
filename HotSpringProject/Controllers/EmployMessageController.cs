7fe4d7dc5d19936b07e84ae25a70fd102f00f6d0
liuxingyu <2108319749@qq.com>
2024-05-15 10:00:06 +0800

    1

---
+++ HotSpringProject/Controllers/EmployMessageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotSpringProject.Controllers
+{
+    public class EmployMessageController : Controller
+    {
+        // GET: EmployMessage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
