c2a640742196428d4b7fc7c8cc6c37b22ddbed70
liuxingyu <2108319749@qq.com>
2024-05-20 14:19:56 +0800

    头像

--- HotSpringProject/DependencyDB/SendMessageDepency.cs
+++ HotSpringProject/DependencyDB/SendMessageDepency.cs
@@ -49,7 +49,6 @@ namespace HotSpringProject.DependencyDB
                     {
                         EmployMessageVO m = new EmployMessageVO();
                         m.send_time = (DateTime?)queryReader["send_time"];
-                        m.link = (string)queryReader["link"];
                         m.part = (string)queryReader["part"];
                         m.sender_id = Convert.ToInt32(queryReader["sender_id"]);
                         m.recipients_id = Convert.ToInt32(queryReader["recipients_id"]);
@@ -68,7 +67,6 @@ namespace HotSpringProject.DependencyDB
                     {
                         EmployMessageVO m = new EmployMessageVO();
                         m.send_time = (DateTime?)sqlReader["send_time"];
-                        m.link = (string)sqlReader["link"];
                         m.part = (string)sqlReader["part"];
                         m.sender_id = Convert.ToInt32(sqlReader["sender_id"]);
                         m.recipients_id = Convert.ToInt32(sqlReader["recipients_id"]);
